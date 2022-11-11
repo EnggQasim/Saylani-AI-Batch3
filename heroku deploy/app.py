c1f60218912116b13a7ead763f139705f09cfcbb
Sir Qasim <m.qasim077@gmail.com>
2022-11-11 22:18:06 +0500

    Add files via upload
    Deployed flask app
    gunicorn
    flask
    sklearn

---
+++ heroku deploy/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.linear_model import LinearRegression
+
+app = Flask(__name__)
+filename='static/finalized_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/model",  methods=['post'])
+def model():
+    result = np.int64(request.form['height'])
+    result = loaded_model.predict([[result]])[0]
+    return f"<h1>Dear Customers your predicted weight is <span style='color:red'>{result}</span></h1>"
+   
+
+if __name__ == '__main__':
+    app.run(debug=True)
